12004120a1d89226037fa8c3d7ee89ce56334e7a
Lucas Souza <lucas@magnastud.io>
2020-01-27 19:31:56 -0300

    Ajustes nos icones das pastas!

--- src/app/tree-manager/shared/components/TreeItem.tsx
+++ src/app/tree-manager/shared/components/TreeItem.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react';
 import { TreeInterface } from '../models/TreeInterface';
 
-import arrow_right from './../icons/btn-expand-folder.svg';
-import arrow_down from './../icons/btn-collapse-folder.svg';
+import { TreeItensTypes } from '../models/TreeItensTypes';
+import { Icon } from './icon/icon';
 
 interface ItemTreeProps {
     itemTree: TreeInterface,
@@ -12,14 +12,19 @@ interface ItemTreeProps {
 export const TreeItem: FC<ItemTreeProps> = ({ itemTree, onClick, paddingLeft }) => {
     return (
         <div
-            onClick={onClick}
             key={itemTree.itemId}
             className="tree-item"
             id={"tree_" + itemTree.itemId}
         >
-            <img src={arrow_down} alt="Arrow down" />
-            <img src={arrow_right} alt="Arrow right" />
-            <div className="item" style={{ paddingLeft: `${paddingLeft}px` }}>{itemTree.itemLabel}</div>
+            <div className="item" style={{ paddingLeft: `${paddingLeft}px` }}>
+                {itemTree.itemType === TreeItensTypes.folder &&
+                    <Icon
+                        onClick={onClick}
+                        iconName={itemTree.nodeExpanded ? "btn-collapse-folder" : "btn-expand-folder"}
+                    />
+                }
+                {itemTree.itemLabel}
+            </div>
         </div>
     );
 }
