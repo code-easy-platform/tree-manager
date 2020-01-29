0be63ffd84d27f373951051dde9a6e0da2372d9d
Lucas Souza <lucas@magnastud.io>
2020-01-28 22:27:43 -0300

    Adicionado o menu de contexto.

--- src/app/tree-manager/shared/components/TreeItem.tsx
+++ src/app/tree-manager/shared/components/TreeItem.tsx
@@ -5,24 +5,24 @@ import { TreeItensTypes } from '../models/TreeItensTypes';
 import { Icon } from './icon/icon';
 
 interface ItemTreeProps {
+    paddingLeft: number,
     itemTree: TreeInterface,
     onSelect(event: React.MouseEvent<HTMLDivElement, MouseEvent>): void | undefined,
-    paddingLeft: number,
+    onContextMenu(event: React.MouseEvent<HTMLDivElement, MouseEvent>): void | undefined,
 }
-export const TreeItem: FC<ItemTreeProps> = ({ itemTree, paddingLeft, onSelect }) => {
+export const TreeItem: FC<ItemTreeProps> = ({ itemTree, paddingLeft, onSelect, onContextMenu }) => {
     return (
         <div
-            onClick={(e: any) => onSelect(e)}
-            key={itemTree.itemId}
             className="tree-item"
+            key={itemTree.itemId}
+            onContextMenu={onContextMenu}
             id={"tree_" + itemTree.itemId}
+            onClick={(e: any) => onSelect(e)}
             style={{ backgroundColor: (itemTree.isSelected ? "#6060a730" : "") }}
         >
             <div className="item" style={{ paddingLeft: `${paddingLeft}px` }}>
                 {itemTree.itemType === TreeItensTypes.folder &&
-                    <Icon
-                        iconName={itemTree.nodeExpanded ? "btn-collapse-folder" : "btn-expand-folder"}
-                    />
+                    <Icon iconName={itemTree.nodeExpanded ? "btn-collapse-folder" : "btn-expand-folder"} />
                 }
                 {itemTree.itemType === TreeItensTypes.file &&
                     <Icon />
