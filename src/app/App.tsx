610af5dd3d192f6f4f60dfcd59ce4b0641c80d91
lucassouza <ls18017@gmail.com>
2020-04-28 21:39:12 -0300

    feat: add event onFocus, add list of base itens, add param "isDisable"
    BREAKING CHANGE: Item base is not allowed, now we have "itens" a list od itens

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,95 +4,111 @@ import { TreeInterface } from './tree-manager/shared/models/TreeInterface';
 import { TreeManager } from './tree-manager/TreeManager';
 import './App.css';
 
-const item: TreeInterface = {
-  id: "0",
-  label: "Item 01",
-  isSelected: false,
-  canDropList:['ITEM'],
-  childs: [
-    {
-      id: "1", label: "Item 02", isSelected: false, canDropList:['ITEM'], childs: [
-        {
-          id: "2", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-            {
-              id: "3", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                {
-                  id: "4", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                    {
-                      id: "5", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                        {
-                          id: "6", label: "Item 03", isSelected: false, canDropList:['ITEM'], childs: [
-                            {
-                              id: "7", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                                {
-                                  id: "8", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                                    {
-                                      id: "9", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                                        {
-                                          id: "10", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                                            {
-                                              id: "11", label: "Item 04", isSelected: false, canDropList:['ITEM'], childs: [
-                                                {
-                                                  id: "12", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                                                    {
-                                                      id: "13", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                                                        {
-                                                          id: "14", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                                                            {
-                                                              id: "15", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                                                                {
-                                                                  id: "16", label: "Item 05", isSelected: false, canDropList:['ITEM'], childs: [
-                                                                    {
-                                                                      id: "17", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                                                                        {
-                                                                          id: "18", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                                                                            {
-                                                                              id: "19", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [
-                                                                                { id: "20", label: "Item 01", isSelected: false, canDropList:['ITEM'], childs: [], type: 'ITEM', nodeExpanded: false }
-                                                                              ], type: 'ITEM', nodeExpanded: false
-                                                                            }
-                                                                          ], type: 'ITEM', nodeExpanded: false
-                                                                        }
-                                                                      ], type: 'ITEM', nodeExpanded: false
-                                                                    }
-                                                                  ], type: 'ITEM', nodeExpanded: false
-                                                                },
-                                                              ], type: 'ITEM', nodeExpanded: false
-                                                            }
-                                                          ], type: 'ITEM', nodeExpanded: false
-                                                        }
-                                                      ], type: 'ITEM', nodeExpanded: false
-                                                    }
-                                                  ], type: 'ITEM', nodeExpanded: false
-                                                }
-                                              ], type: 'ITEM', nodeExpanded: false
-                                            },
-                                          ], type: 'ITEM', nodeExpanded: false
-                                        }
-                                      ], type: 'ITEM', nodeExpanded: false
-                                    }
-                                  ], type: 'ITEM', nodeExpanded: false
-                                }
-                              ], type: 'ITEM', nodeExpanded: false
-                            }
-                          ], type: 'ITEM', nodeExpanded: false
-                        },
-                      ], type: 'ITEM', nodeExpanded: false
-                    }
-                  ], type: 'ITEM', nodeExpanded: false
-                }
-              ], type: 'ITEM', nodeExpanded: false
-            }
-          ], type: 'ITEM', nodeExpanded: false, isDisabledSelect: true,
-        }
-      ], type: 'ITEM', nodeExpanded: false,  isAllowedToggleNodeExpand: true, isDisabledSelect: false
-    },
-    { id: "21", label: "Item 03", isSelected: false, canDropList:['ITEM'], childs: [], type: 'ITEM', nodeExpanded: false, isDisabledDrag: false, isDisabledSelect: true },
-    { id: "22", label: "Item 04", isSelected: false, canDropList:['ITEM'], childs: [], type: 'ITEM', nodeExpanded: false, isDisabledDrag: true, isDisabledDrop: true, isDisabledSelect: false, hasError: true },
-  ],
-  type: 'ITEM',
-  nodeExpanded: false
-};
+const itensBase: TreeInterface[] = [
+  {
+    id: "0",
+    label: "Item 01",
+    isSelected: false,
+    isDisabledSelect: true,
+    canDropList: ['ITEM'],
+    childs: [
+      {
+        id: "1", label: "Item 02", isSelected: false, canDropList: ['ITEM'], childs: [
+          {
+            id: "2", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+              {
+                id: "3", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                  {
+                    id: "4", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                      {
+                        id: "5", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                          {
+                            id: "6", label: "Item 03", isSelected: false, canDropList: ['ITEM'], childs: [
+                              {
+                                id: "7", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                                  {
+                                    id: "8", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                                      {
+                                        id: "9", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                                          {
+                                            id: "10", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                                              {
+                                                id: "11", label: "Item 04", isSelected: false, canDropList: ['ITEM'], childs: [
+                                                  {
+                                                    id: "12", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                                                      {
+                                                        id: "13", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                                                          {
+                                                            id: "14", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                                                              {
+                                                                id: "15", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                                                                  {
+                                                                    id: "16", label: "Item 05", isSelected: false, canDropList: ['ITEM'], childs: [
+                                                                      {
+                                                                        id: "17", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                                                                          {
+                                                                            id: "18", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                                                                              {
+                                                                                id: "19", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [
+                                                                                  { id: "20", label: "Item 01", isSelected: false, canDropList: ['ITEM'], childs: [], type: 'ITEM', nodeExpanded: false }
+                                                                                ], type: 'ITEM', nodeExpanded: false
+                                                                              }
+                                                                            ], type: 'ITEM', nodeExpanded: false
+                                                                          }
+                                                                        ], type: 'ITEM', nodeExpanded: false
+                                                                      }
+                                                                    ], type: 'ITEM', nodeExpanded: false
+                                                                  },
+                                                                ], type: 'ITEM', nodeExpanded: false
+                                                              }
+                                                            ], type: 'ITEM', nodeExpanded: false
+                                                          }
+                                                        ], type: 'ITEM', nodeExpanded: false
+                                                      }
+                                                    ], type: 'ITEM', nodeExpanded: false
+                                                  }
+                                                ], type: 'ITEM', nodeExpanded: false
+                                              },
+                                            ], type: 'ITEM', nodeExpanded: false
+                                          }
+                                        ], type: 'ITEM', nodeExpanded: false
+                                      }
+                                    ], type: 'ITEM', nodeExpanded: false
+                                  }
+                                ], type: 'ITEM', nodeExpanded: false
+                              }
+                            ], type: 'ITEM', nodeExpanded: false
+                          },
+                        ], type: 'ITEM', nodeExpanded: false
+                      }
+                    ], type: 'ITEM', nodeExpanded: false
+                  }
+                ], type: 'ITEM', nodeExpanded: false
+              }
+            ], type: 'ITEM', nodeExpanded: false, isDisabled: true,
+          }
+        ], type: 'ITEM', nodeExpanded: false, isAllowedToggleNodeExpand: true, isDisabled: false
+      },
+      { id: "21", label: "Item 03", isSelected: false, canDropList: ['ITEM'], childs: [], type: 'ITEM', nodeExpanded: false, isDisabledDrag: false, isDisabled: true },
+      { id: "22", label: "Item 04", isSelected: false, canDropList: ['ITEM'], childs: [], type: 'ITEM', nodeExpanded: false, isDisabledDrag: true, isDisabledDrop: true, isDisabled: false, hasError: true },
+    ],
+    type: 'ITEM',
+    nodeExpanded: false
+  },
+  {
+    id: "0",
+    label: "Item 01",
+    isSelected: false,
+    nodeExpanded: false,
+    canDropList: ['ITEM'],
+    isDisabledSelect: true,
+    childs: [
+      { id: "30", label: "Item 30", isSelected: false, canDropList: ['ITEM'], childs: [], type: 'ITEM', nodeExpanded: false, isDisabledDrag: false, isDisabled: true },
+      { id: "31", label: "Item 31", isSelected: false, canDropList: ['ITEM'], childs: [], type: 'ITEM', nodeExpanded: false, isDisabledDrag: true, isDisabledDrop: true, isDisabled: false, hasError: true },
+    ],
+    type: 'ITEM',
+  }
+];
 
 const App: FC = () => {
   return (
@@ -101,9 +117,10 @@ const App: FC = () => {
         <div style={{ width: 300, height: 500, alignSelf: "center", backgroundColor: "#1E1E1E", flexDirection: "column" }}>
 
           <TreeManager
-            itemBase={item}
             isUseDrag={true}
             isUseDrop={true}
+            itens={itensBase}
+            onFocus={e => { console.log(e) }}
             onClick={(id: string) => { console.log(id) }}
             onContextMenu={(id, e) => { e.preventDefault() }}
             onDoubleClick={(id, item, e) => { console.log(id) }}
