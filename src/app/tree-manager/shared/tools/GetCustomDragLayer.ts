821f322d39c020fa0ad4bea7eae63ae1d0cac0d3
lvargas <lvargas@gvdasa.com.br>
2021-05-17 12:53:38 -0300

    refactor: changed custom drag layer

--- src/app/tree-manager/shared/tools/GetCustomDragLayer.ts
+++ src/app/tree-manager/shared/tools/GetCustomDragLayer.ts
@@ -1,9 +1,8 @@
-
 interface IOptions {
   color?: string;
   icon?: string;
 }
-export const getCustomDragLayer = (text: string, options?: IOptions): HTMLLabelElement => {
+export const getCustomDragLayer = (text: string, options?: IOptions): HTMLElement => {
   const container = document.createElement('label');
 
   container.style.padding = '4px';
@@ -19,20 +18,23 @@ export const getCustomDragLayer = (text: string, options?: IOptions): HTMLLabelE
   container.style.position = 'fixed';
   container.style.display = 'flex';
   container.style.zIndex = '-100';
+  container.style.left = '-100px';
+  container.style.top = '-100px';
 
+  
   if (options?.icon && options?.icon.includes('data')) {
     const icon = document.createElement('img');
-
+    
     icon.width = 16;
     icon.alt = text;
     icon.height = 16;
     icon.src = options?.icon;
     icon.style.marginRight = '4px';
     icon.style.filter = 'brightness(2)';
-
+    
     container.appendChild(icon);
   }
-
+  
   container.appendChild(new Text(text));
 
 
